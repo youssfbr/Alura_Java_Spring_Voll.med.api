1fb4c2ea014d62d9362dacba8164177b954e4c06
Alisson Youssf <youssfbr@gmail.com>
2024-11-26 21:09:55 -0300

    Mensagens de validacão

--- src/main/java/com/github/youssfbr/voll/med/api/domains/medicos/dtos/DadosCadastroMedicoDTO.java
+++ src/main/java/com/github/youssfbr/voll/med/api/domains/medicos/dtos/DadosCadastroMedicoDTO.java
@@ -9,28 +9,28 @@ public record DadosCadastroMedicoDTO(
 
         Long id ,
 
-        @NotBlank @Size(max = 100)
+        @NotBlank @Size(max = 100 , message = "Nome é obrigatório")
         String nome ,
 
-        @Email
-        @NotBlank
+        @Email(message = "Email é obrigatório")
+        @NotBlank(message = "Email é obrigatório")
         @Size(max = 100)
         String email ,
 
         @NotBlank
-        @Pattern(regexp = "\\d{4,6}")
+        @Pattern(regexp = "\\d{4,6}" , message = "Formato do CRM é inválido")
         String crm ,
 
-        @NotNull
+        @NotNull(message = "Especialidade é obrigatória")
         Especialidade especialidade ,
 
-        @NotBlank
+        @NotBlank(message = "Número do WhatsApp é obrigatório")
         String whatsapp ,
 
         String telefone ,
 
         @Valid
-        @NotNull
+        @NotNull(message = "Dados do endereço são obrigatórios")
         DadosEnderecoDTO endereco
 ) {
 }
