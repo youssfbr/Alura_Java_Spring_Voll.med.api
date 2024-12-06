8569d72cf16208dc1675d74715206df9c7972af8
Alisson Youssf <youssfbr@gmail.com>
2024-12-06 17:28:58 -0300

    Pegando o token no cabeçalho da requisição

--- src/main/java/com/github/youssfbr/voll/med/api/infra/security/SecurityFilter.java
+++ src/main/java/com/github/youssfbr/voll/med/api/infra/security/SecurityFilter.java
@@ -14,6 +14,18 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request , HttpServletResponse response , FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request , response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        final String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
+        }
+        return authorizationHeader.replace("Bearer " , "");
     }
 }
